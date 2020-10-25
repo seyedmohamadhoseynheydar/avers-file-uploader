76b8fccfa7c8e75be22b7808459ec3b9861f1b0c
seyedseyedmohamadhoseynheydarrabooki <72008666+seyedmohamadhoseynheydar@users.noreply.github.com>
2020-10-25 13:44:32 +0330

    Update and rename videoUploader.php to VideoUploader.php

---
+++ src/VideoUploader.php
@@ -0,0 +1,58 @@
+<?php
+
+
+namespace avers\aversFileUploader;
+
+use Yii;
+use yii\base\Widget;
+use yii\helpers\Html;
+use yii\web\View;
+
+class VideoUploader extends Widget
+{
+
+
+
+    public $form;
+    public $form_name;
+    public $form_name_capital;
+    public $model;
+
+
+    public function init()
+    {
+        parent::init();
+        Asset::register( $this->getView() );
+    }
+
+    public function run()
+    {
+        parent::init();
+        
+        $html = '';
+        $html .= '<label>';
+        $html .= '' . Yii::t('app', 'upload main video') . '';
+        $html .= '</label>';
+        $html .= '<span class="hidden">';
+        $html .= '' . $this->form->field($this->model, 'video_id', ['template' => '{input}'])->hiddenInput() . '';
+        $html .= '</span>';
+        $html .= '<span class="hidden">';
+        $html .= '' . $this->form->field($this->model, 'main_video')->fileInput(['onchange' => 'uploadImage("' . $this->form_name . '","' . $this->form_name_capital . '")']) . '';
+        $html .= '</span>';
+        $html .= '<button onclick=\'openUploadVideo("' . $this->form_name . '")\' type="button" class="btn btn-primary btn-sm ml-4px">
+                        <i class="fa fa-upload"></i>
+                  </button>';
+        $html .= '<button id="remove-video" type="button" class="btn btn-danger btn-sm">
+                        <i class="fa fa-close"></i>
+                  </button>';
+        $html .= '<div id="main-video" style="margin-top: 10px">';
+        if ($this->model->video_id) {
+            $html .= '' . $this->model->getVideoTag() . '';
+        }
+        $html .= '</div>';
+        $html .= '<p class="text-muted">';
+        $html .= '(' . Yii::t("app", "Maximum 30 MB") . ')';
+        $html .= '</p>';
+        return $html;
+    }
+}
