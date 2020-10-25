2e1d53f0ff3db99e8e32a449604a28051c67bef6
seyedseyedmohamadhoseynheydarrabooki <72008666+seyedmohamadhoseynheydar@users.noreply.github.com>
2020-10-25 14:04:57 +0330

    Update Asset.php

--- src/Asset.php
+++ src/Asset.php
@@ -1,11 +1,11 @@
 <?php
-namespace avers\aversFileUploader;
+namespace avers\fileUploader;
 
 use yii\web\AssetBundle;
 
 class Asset extends AssetBundle {
 
-    public $sourcePath = '@vendor/avers-file-uploader/file-uploader/src';
+    public $sourcePath = '@vendor/avers/file-uploader/src';
     public $js = ['assets/js/file-uploader.js'];
     public $css = ['assets/css/file-uploader.css'];
     public $depends = [
