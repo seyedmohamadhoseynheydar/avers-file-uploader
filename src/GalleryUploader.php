977d69308064dd382b09046ce3ea97e798108716
seyedseyedmohamadhoseynheydarrabooki <72008666+seyedmohamadhoseynheydar@users.noreply.github.com>
2021-04-24 15:37:50 +0430

    Update GalleryUploader.php

--- src/GalleryUploader.php
+++ src/GalleryUploader.php
@@ -82,7 +82,7 @@ class GalleryUploader extends Widget
                                 $checked = "";
                             }
                             $html .= ' <diV><input '.$checked.' class="form-check-input" type="radio" name="'.$this->form_name_capital.'[image_id]" value="'.$file->id.'" id="main_image_'.$file->id.'">';
-                            $html .= '<label for="main_image_'.$file->id.'">تصویر اصلی</label></diV>';
+                            $html .= '</diV><div><label for="main_image_'.$file->id.'">تصویر اصلی</label></diV>';
                         }
                         $html .= '<img style="" src="'.$this->model->getImageUriById($file->id, null, 150, File::RESIZE_INSIDE) .'">';
                         $html .= '</div>';
