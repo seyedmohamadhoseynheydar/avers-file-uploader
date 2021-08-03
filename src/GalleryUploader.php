e72441a444d6bd5de4dc1e0f6f5a6026db889d68
seyedseyedmohamadhoseynheydarrabooki <72008666+seyedmohamadhoseynheydar@users.noreply.github.com>
2021-08-03 13:46:58 +0430

    Update GalleryUploader.php

--- src/GalleryUploader.php
+++ src/GalleryUploader.php
@@ -114,10 +114,8 @@ class GalleryUploader extends Widget
             }
         }
         if ($this->skin == 'bazar-rouz-iranian'){
-            $html .= '<div class="col-md-4" style="margin-top: 10px;margin-bottom: 10px">';
-            $html .= '<button onclick=\'openMultiUpload("' . $this->form_name . '")\' type="button" class="btn" style="padding: 70px 70px;border: 1px solid #18E3A4;border-radius: 10px">
-                        <i class="fa fa-plus-square-o" style="color: #18E3A4; font-size: 100px"></i>
-                  </button>';
+            $html .= '<div onclick=\'openMultiUpload("' . $this->form_name . '")\' class="col-md-4 fa fa-plus-square-o" style="color: #18E3A4;margin-top: 10px;margin-bottom: 10px;border: 1px solid #18E3A4;border-radius: 10px">';        
+                    <i class="fa fa-plus-square-o" style="color: #18E3A4; font-size: 100px"></i>               
             $html .= '</div>';
         }
         $html .= '</div>';
